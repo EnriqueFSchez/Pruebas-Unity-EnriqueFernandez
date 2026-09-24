9e5300a44af0217ddb03b383a4f33c04722adf35
EnriqueFSchez <henry.fschez@gmail.com>
2026-09-24 13:17:43 +0200

    movimiento personaje

--- Pruebas-unity/Assets/MyAssets_/01_scripts/playerManager.cs
+++ Pruebas-unity/Assets/MyAssets_/01_scripts/playerManager.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+    [SerializeField] float speedZ;
+    [SerializeField] float speedX;
+    [SerializeField] float rotSpeed;
+
+    float moveZ;
+    float moveX;
+
+
+    [SerializeField] float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+    Vector3 currentRot;
+
+
+    private void Awake()
+    {
+
+        Inputtions = new InputPlayer_Acciones();
+
+
+
+    }
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
     }
+
+
+
+
+
 }
